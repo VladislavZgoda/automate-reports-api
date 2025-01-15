c4c319f1c4bc12adf1729e47295c81e60688c402
zgodavg <www.vlad78@yandex.ru>
2025-01-15 16:32:51 +0300

    Start testing express

--- index.ts
+++ index.ts
@@ -1,45 +1,36 @@
-import Fastify, { FastifyInstance } from "fastify";
-import { fastifyMultipart } from "@fastify/multipart";
-import { createWriteStream } from "node:fs";
-import { pipeline } from "stream/promises";
-import { folderExists } from "utils/fileSystemFunc.js";
-import { randomUUID } from "crypto";
+import express from "express";
+import multer from "multer";
 import parseMatritca from "parse-excel/parseMatritca.js";
 
-const fastify: FastifyInstance = Fastify({
-  logger: true,
+const app = express();
+const port = 3000;
+
+const storage = multer.diskStorage({
+  destination: function (_req, _file, cb) {
+    cb(null, "./upload/");
+  },
+  filename: function (_req, file, cb) {
+    cb(null, file.originalname);
+  },
 });
 
-fastify.register(fastifyMultipart);
+const upload = multer({ storage: storage });
 
-fastify.post("/api/matritca/", async function handler(request, reply) {
-  const data = await request.file();
+app.post("/api/matritca/", upload.single("upload"), async (req, res) => {
+  const fileName = req.file?.originalname;
+  const balanceGroup = req.body.balanceGroup;
+  const folder = import.meta.dirname + '/parsed-excel/';
 
-  if (
-    data?.mimetype !==
-    "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
-  ) {
-    reply.code(406).send({
-      message:
-        "Only 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' content types supported.",
-    });
-  }
+  const downloadFileName = `Приложение №9 ${balanceGroup === "private" ? "Быт" : "Юр"}.xlsx`;
 
-  if (data?.file) {
-    const fileName = `private_sector${randomUUID()}.xlsx`;
-    await folderExists("./upload");
-    await pipeline(data.file, createWriteStream(`./upload/${fileName}`));
+  await parseMatritca(fileName, balanceGroup);
 
-    // @ts-expect-error некорректный тип, код рабочий
-    const balanceGroup = data?.fields.balanceGroup.value as "private" | "legal";
-    await parseMatritca(fileName, balanceGroup);
-    reply.code(200).send();
-  }
+  // res.setHeader("Content-Disposition", `attachment; filename=${downloadFileName}`);
+  // res.setHeader('Content-type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+
+  res.download(folder + downloadFileName);
 });
 
-try {
-  await fastify.listen({ port: 3000 });
-} catch (err) {
-  fastify.log.error(err);
-  process.exit(1);
-}
+app.listen(port, () => {
+  console.log(`Automate-reports-api app listening on port ${port}`);
+});
