6ec266279f762c703209823805e6e3efd5085857
Garry3Fingers <www.vlad78@yandex.ru>
2025-01-16 19:53:17 +0300

    Refactor encodeURIComponent

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import multer from "multer";
-import cors from 'cors';
+import cors from "cors";
 import exceljs from "exceljs";
 import parseMatritca from "parse-excel/parseMatritca.js";
 
@@ -22,8 +22,7 @@ const upload = multer({ storage: storage });
 app.post("/api/matritca/", upload.single("upload"), async (req, res) => {
   const fileName = req.file?.originalname;
   const balanceGroup = req.body.balanceGroup;
-  // const folder = import.meta.dirname + '/parsed-excel/';
-  // const downloadFileName = `Приложение №9 ${balanceGroup === "private" ? "Быт" : "Юр"}.xlsx`;
+  const downloadFileName = `Приложение №9 ${balanceGroup === "private" ? "Быт" : "Юр"}.xlsx`;
 
   const excel = new exceljs.Workbook();
   const wb = await excel.xlsx.readFile(`upload/${fileName}`);
@@ -32,17 +31,15 @@ app.post("/api/matritca/", upload.single("upload"), async (req, res) => {
 
   res.setHeader(
     "Content-Type",
-    "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+    "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
   );
-
-  const encoded = encodeURI("Приложение №9 ЮР.xlsx");
 
   res.setHeader(
     "Content-Disposition",
-    `attachment; filename*=UTF-8''${encoded}; filename="${encoded}"`
+    `attachment; filename="${encodeURIComponent(downloadFileName)}"`,
   );
-  
-  wb.xlsx.write(res).then(() => res.status(200).end())
+
+  wb.xlsx.write(res).then(() => res.status(200).end());
 });
 
 app.listen(port, () => {
