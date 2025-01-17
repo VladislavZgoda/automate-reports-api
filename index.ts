f322f9cd332db582345837f8c7fecabdee1fa6b6
zgodavg <www.vlad78@yandex.ru>
2025-01-17 13:55:46 +0300

    Add validation for request form data

--- index.ts
+++ index.ts
@@ -22,7 +22,24 @@ const storage = multer.diskStorage({
 const upload = multer({ storage: storage });
 
 app.post("/api/matritca/", upload.single("upload"), async (req, res) => {
-  const fileName = req.file?.filename;
+  if (!req.file) {
+    res.status(400).send("The form data is missing a xlsx file.");
+  } else if (
+    req.file.mimetype !==
+    "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+  ) {
+    deleteFile(req.file.filename);
+    res
+      .status(415)
+      .send(
+        "Only 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' content types supported.",
+      );
+  } else if (!["private", "legal"].includes(req.body.balanceGroup)) {
+    deleteFile(req.file.filename);
+    res.status(400).send("The form data is missing a balance group.");
+  }
+
+  const fileName = req.file?.filename as string;
   const balanceGroup = req.body.balanceGroup;
   const downloadFileName = `Приложение №9 ${balanceGroup === "private" ? "Быт" : "Юр"}.xlsx`;
 
