4a452fe2d96ee73c63cfea3a0fc3f9674ff4132f
zgodavg <www.vlad78@yandex.ru>
2025-01-29 16:43:51 +0300

    Continue implementing func fillOdpyTemplate

--- src/parse-excel/fillOdpyTemplate.ts
+++ src/parse-excel/fillOdpyTemplate.ts
@@ -1,10 +1,11 @@
 import exceljs from "exceljs";
+import { todayDate } from "src/utils/dateFunc.ts";
 
 type Meters = {
   [serialNumber: string]: {
-    t1: string;
-    t2: string;
-    t: string;
+    t1: number;
+    t2: number;
+    t: number;
     date: string;
   };
 };
@@ -28,37 +29,42 @@ export default async function fillOdpyTemplate(
 
   const meters: Meters = {};
   parsePiramidaOdpy(wsPiramida, meters);
-  parseMatritcaOdpy(wsMatritca, meters); 
+  parseMatritcaOdpy(wsMatritca, meters);
+
+  wsTemplate.removeConditionalFormatting("");
+  fillTemplate(wsTemplate, meters);
+
+  await wbTemplate.xlsx.writeFile(`parsed-excel/test.xlsx`);
 }
 
 function parsePiramidaOdpy(ws: exceljs.Worksheet, meters: Meters) {
   for (let i = 6; i < ws.actualRowCount + 1; i++) {
     if (ws.getCell("P" + i).value) {
       meters[String(ws.getCell("C" + i).value)] = {
-        t1: String(ws.getCell("Q" + i).value),
-        t2: String(ws.getCell("R" + i).value),
-        t: String(ws.getCell("P" + i).value),
+        t1: Number(ws.getCell("Q" + i).value),
+        t2: Number(ws.getCell("R" + i).value),
+        t: Number(ws.getCell("P" + i).value),
         date: String(ws.getCell("P4").value),
       };
     } else if (ws.getCell("L" + i).value) {
       meters[String(ws.getCell("C" + i).value)] = {
-        t1: String(ws.getCell("M" + i).value),
-        t2: String(ws.getCell("N" + i).value),
-        t: String(ws.getCell("L" + i).value),
+        t1: Number(ws.getCell("M" + i).value),
+        t2: Number(ws.getCell("N" + i).value),
+        t: Number(ws.getCell("L" + i).value),
         date: String(ws.getCell("L4").value),
       };
     } else if (ws.getCell("H" + i).value) {
       meters[String(ws.getCell("C" + i).value)] = {
-        t1: String(ws.getCell("I" + i).value),
-        t2: String(ws.getCell("J" + i).value),
-        t: String(ws.getCell("H" + i).value),
+        t1: Number(ws.getCell("I" + i).value),
+        t2: Number(ws.getCell("J" + i).value),
+        t: Number(ws.getCell("H" + i).value),
         date: String(ws.getCell("H4").value),
       };
     } else if (ws.getCell("D" + i).value) {
       meters[String(ws.getCell("C" + i).value)] = {
-        t1: String(ws.getCell("E" + i).value),
-        t2: String(ws.getCell("F" + i).value),
-        t: String(ws.getCell("D" + i).value),
+        t1: Number(ws.getCell("E" + i).value),
+        t2: Number(ws.getCell("F" + i).value),
+        t: Number(ws.getCell("D" + i).value),
         date: String(ws.getCell("D4").value),
       };
     }
@@ -75,12 +81,37 @@ function parseMatritcaOdpy(ws: exceljs.Worksheet, meters: Meters) {
       let serialNumber = String(ws.getCell("C" + i).value).trim();
       if (serialNumber.length === 7) serialNumber = "0" + serialNumber;
 
+      const date = ws.getCell("D" + i).value as Date;
+      const localDateFormat = new Date(date).toLocaleDateString("ru");
+
       meters[serialNumber] = {
-        t1: String(ws.getCell("E" + i).value),
-        t2: String(ws.getCell("F" + i).value),
-        t: String(ws.getCell("H" + i).value),
-        date: String(ws.getCell("D" + i).value),
+        t1: Number(ws.getCell("E" + i).value),
+        t2: Number(ws.getCell("F" + i).value),
+        t: Number(ws.getCell("H" + i).value),
+        date: localDateFormat,
       };
     }
   }
+}
+
+function fillTemplate(ws: exceljs.Worksheet, meters: Meters) {
+  const askueDate = todayDate();
+
+  for (let i = 3; i < ws.actualRowCount + 1; i++) {
+    const serialNumber = String(ws.getCell("C" + i).value).trim();
+
+    if (Object.hasOwn(meters, serialNumber)) {
+      ws.getCell("D" + i).value = meters[serialNumber].date;
+      handleActivePower(ws, `E${i}`, meters[serialNumber].t1);
+      handleActivePower(ws, `F${i}`, meters[serialNumber].t2);
+      handleActivePower(ws, `H${i}`, meters[serialNumber].t);
+      ws.getCell("K" + i).value = askueDate;
+    }
+  }
+}
+
+function handleActivePower(ws: exceljs.Worksheet, cell: string, value: number) {
+  const currentCell = ws.getCell(cell);
+  currentCell.numFmt = "@";
+  currentCell.value = value.toFixed(2);
 }
