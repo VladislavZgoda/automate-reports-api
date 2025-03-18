272b5de593dcbe4147304eaff32f8941ca773e21
zgodavg <www.vlad78@yandex.ru>
2025-03-18 15:35:29 +0300

    Change config

--- eslint.config.js
+++ eslint.config.js
@@ -9,6 +9,15 @@ export default [
   {files: ["**/*.{js,mjs,cjs,ts}"]},
   {languageOptions: { globals: globals.node }},
   pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  ...tseslint.configs.stylisticTypeChecked,
   eslintConfigPrettier,
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
 ];
